27967b44c559db9ed10e7baee9fe4a52db8a75df
sukritichawla <sukritichawla77@gmail.com>
2025-11-08 21:51:08 +0530

    Emails sent

--- backend/src/routes/uploads.js
+++ backend/src/routes/uploads.js
@@ -1,16 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
 import multer from "multer";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { dyDB_client } from "../middleware/dynamo_config.js";
 import { s3_client } from "../middleware/s3_config.js";
+import AWS from "aws-sdk";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, "../.env") });
 
 dotenv.config();
+console.log("🔍 ENV CHECK:", process.env.AWS_ACCESS_KEY_ID, process.env.AWS_REGION);
+
 const router = express.Router();
 
 // AWS clients
 const ddb = DynamoDBDocumentClient.from(dyDB_client);
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+
+const sns = new AWS.SNS();
+
+async function notifyUpload(title, artist, email) {
+  const params = {
+    Message: `🎵 New song uploaded!\nTitle: ${title}\nArtist: ${artist}\nUploaded by: ${email}`,
+    TopicArn: "arn:aws:sns:ap-south-1:050963339477:songUploadNotifications"
+  };
+  await sns.publish(params).promise();
+  console.log("✅ SNS Email notification sent!");
+}
 
 const SONGS_TABLE = "Songs-Napster-DB";
 const S3_BUCKET = "napster-bucket-123456"; // replace with your actual bucket name
@@ -92,6 +119,8 @@ router.post(
           Item: songItem,
         })
       );
+
+      await notifyUpload(title, artist, email);
 
       res.status(201).json({
         message: "Song uploaded successfully",
